e664b321907d7aa51165f94f67fe07b20689aaa2
Brandi Hornbuckle <brandi.hornbuckle@gmail.com>
2022-04-01 00:29:10 -0400

    Setting up an express server.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Express server
-let express = require('express')
-let app = express();
-let router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
+
 
 router.get('/', function (req, res, next) {
-    res.send("dragon");
+    res.status(200).send("dragon");
 });
 
 app.use('/api/', router);
@@ -13,3 +14,6 @@ var server = app.listen(5000, function () {
     console.log('Node server is running on http://localhost:5000..');
 });
 
+app.get("https://www.dnd5eapi.co/api/spells/", (req, res) => {
+    console.log('name');
+});
