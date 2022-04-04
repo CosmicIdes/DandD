497fbb1f1370c678b1106089c8304339b792264a
Brandi Hornbuckle <brandi.hornbuckle@gmail.com>
2022-04-04 17:41:19 -0400

    Disabling CORS as it is blocking the JSON files.

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const cors = require('cors');
 
 router.get('/', function (req, res, next) {
     res.status(200).send("dragon");
 });
 
 app.use('/api/', router);
-
-app.use(cors());
 
 var server = app.listen(5000, function () {
     console.log('Node server is running on http://localhost:5000..');
